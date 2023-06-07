e3806002cb8b84a6f71573ed3f1123049e871081
Artur Costa <souza.artur@edu.univali.br>
2023-06-07 15:05:05 -0300

    virtual e override de atributos

--- src/Univali.Api/Models/CustomerForManipulationDto.cs
+++ src/Univali.Api/Models/CustomerForManipulationDto.cs
@@ -7,9 +7,8 @@ public abstract class CustomerForManipulationDto
     [Required(ErrorMessage = "You should fill out a Name")]
     [MaxLength(100, ErrorMessage = "The name shouldn't have more than 100 characters")]
     public string Name {get; set;} = string.Empty;
-
     [Required(ErrorMessage = "You should fill out a Cpf")]
     [StringLength(11, MinimumLength = 11, ErrorMessage = 
     "The Cpf should have 11 characters")]
-    public string Cpf {get; set;} = string.Empty;
+    public virtual string Cpf {get; set;} = string.Empty;
 }
--- src/Univali.Api/Models/CustomerForUpdateDto.cs
+++ src/Univali.Api/Models/CustomerForUpdateDto.cs
@@ -6,4 +6,5 @@ public class CustomerForUpdateDto : CustomerForManipulationDto
 {
     [Required(ErrorMessage = "You should fill out an Id")]
     public int Id {get; set;}
+    public override string Cpf {get; set;} = string.Empty;
 }
